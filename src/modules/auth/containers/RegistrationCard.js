99294267705c4f55951ca2948e556c1c1fb9369c
Pe5h4 <pesekf@email.cz>
2022-12-07 17:42:42 +0100

    Add registration formfields

--- src/modules/auth/containers/RegistrationCard.js
+++ src/modules/auth/containers/RegistrationCard.js
@@ -11,10 +11,18 @@ import {
 	Button
 } from 'reactstrap';
 
+import {
+	PhoneField,
+	EmailField,
+	PasswordField,
+	UserNameField
+} from './FormFields';
+
 // TODO: Registration card has not been implemented yet
 function RegistrationCard(props) {
 	const { t, i18n } = useTranslation();
-	const { handleSubmit, register, getValues, formState: { errors, isSubmitting } } = useForm();
+
+	const { handleSubmit, register, getValues, setValue, formState: { errors, isSubmitting } } = useForm();
 
 	const onSubmit = values => {
 		// TODO
@@ -27,13 +35,21 @@ function RegistrationCard(props) {
 					<div className="card-header-title" >
 						<CardTitle className="text-primary" tag="h2">{t('RegistrationCard|New user?')}</CardTitle>
 						<CardSubtitle tag="p">
-							{t('New users, please register here')}
+							{t('If you are a new users, please register here')}
 						</CardSubtitle>
 					</div>
 				</CardHeader>
 				{/*TODO*/}
 				<CardBody>
-					...
+					{props.registerFeatures && props.registerFeatures?.credentials && Object.keys(props.registerFeatures?.credentials).map((key, idx) => {
+						switch(key) {
+							case 'username': return(<UserNameField key={idx} content={props.registerFeatures?.credentials?.username} register={register} getValues={getValues} errors={errors} />)
+							case 'email': return(<EmailField key={idx} content={props.registerFeatures?.credentials?.email} register={register} getValues={getValues} errors={errors} />)
+							case 'password': return(<PasswordField key={idx} content={props.registerFeatures?.credentials?.password} register={register} getValues={getValues} errors={errors} />)
+							case 'phone': return(<PhoneField key={idx} content={props.registerFeatures?.credentials?.phone} register={register} getValues={getValues} setValue={setValue} errors={errors} />)
+							default: return(<div key={idx}>Unknown item: "{key}"</div>)
+						}
+					})}
 
 					<Row className="justify-content-center">
 						<Col>
