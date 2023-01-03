5787a72810ddbdcd9604af7f219c377adb1ca115
Petr Kavulok <petrkavulok@Petrs-MacBook-Pro.local>
2023-01-03 14:37:29 +0100

    speedMeasurePlugin installation

--- asab-webui.config.js
+++ asab-webui.config.js
@@ -1,3 +1,6 @@
+
+const SpeedMeasurePlugin = require("speed-measure-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 /**
  * Uncomment BundleAnalyzerPlugin and add it to extraPlugins in case
  * you need to measure bundle size but don't push it to master.
@@ -12,7 +15,7 @@
  * */
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
-module.exports = {
+const config = {
 	extraEntries: { /* add more entries here */ },
 	extraOutputs: { /* add more outputs here */ },
 	extraPlugins: [ /* add more plugins here */ ],
@@ -21,3 +24,9 @@ module.exports = {
 		extraRules: [ /* add more module rules here */ ]
 	}
 };
+
+
+const configWithTimeMeasures = new SpeedMeasurePlugin().wrap(config);
+configWithTimeMeasures.plugins.push(new MiniCssExtractPlugin({}));
+
+module.exports = configWithTimeMeasures;
