dd7060f93e24d4a79384e246b1128d4089c1260c
robin.hruska@teskalabs.com <byewokko@seznam.cz>
2024-05-10 10:05:16 +0200

    password criteria wip

--- src/modules/auth/passwd/ResetPwdScreen.js
+++ src/modules/auth/passwd/ResetPwdScreen.js
@@ -9,6 +9,14 @@ import {
 	Form, FormGroup, FormText, Label, Input, Button, FormFeedback
 } from 'reactstrap';
 import generatePenrose from '../utils/generatePenrose';
+import {
+	validatePasswordLength,
+	validatePasswordLowercaseCount,
+	validatePasswordUppercaseCount,
+	validatePasswordDigitCount,
+	validatePasswordSpecialCount,
+	PasswordCriteriaFeedback,
+} from '../utils/passwordValidation';
 
 function ResetPwdScreen(props) {
 
@@ -28,13 +36,51 @@ export default ResetPwdScreen;
 
 function ResetPwdCard(props) {
 	const { t, i18n } = useTranslation();
-	const { handleSubmit, register, getValues, formState: { errors, isSubmitting } } = useForm();
+	const SeaCatAuthAPI = props.app.axiosCreate('seacat-auth');
+	const { handleSubmit, register, getValues, watch, formState: { errors, isSubmitting } } = useForm();
 
 	generatePenrose();
 	
 	let history = useHistory();
 
 	const [ completed, setCompleted ] = useState(false);
+	const [ passwordCriteria, setPasswordCriteria ] = useState({
+		minLength: 10,
+	});
+
+	useEffect(() => {
+		loadPasswordCriteria();
+	}, []);
+
+	const loadPasswordCriteria = async () => {
+		try {
+			const response = await SeaCatAuthAPI.get('/public/password/policy');
+			setPasswordCriteria({
+				minLength: response.data?.min_length,
+				minLowercaseCount: response.data?.min_lowercase_count,
+				minUppercaseCount: response.data?.min_uppercase_count,
+				minDigitCount: response.data?.min_digit_count,
+				minSpecialCount: response.data?.min_special_count,
+			});
+		} catch (e) {
+			if (e?.response?.status == 404) {
+				// Most likely older service version which does not have this endpoint
+				console.error(e);
+			} else {
+				props.app.addAlertFromException(e, t('ChangePwdScreen|Failed to load password criteria'));
+			}
+		}
+	};
+
+	// Password is watched for immediate feedback to the user
+	const watchedNewPassword = watch('newpassword', '');
+	const validateNewPassword = (value) => ({
+		minLength: validatePasswordLength(value, passwordCriteria?.minLength),
+		minLowercaseCount: validatePasswordLowercaseCount(value, passwordCriteria?.minLowercaseCount),
+		minUppercaseCount: validatePasswordUppercaseCount(value, passwordCriteria?.minUppercaseCount),
+		minDigitCount: validatePasswordDigitCount(value, passwordCriteria?.minDigitCount),
+		minSpecialCount: validatePasswordSpecialCount(value, passwordCriteria?.minSpecialCount),
+	});
 
 	const isButtonRemoved = props.app.Config.get('password_change')?.remove_btn;
 
@@ -45,7 +91,8 @@ function ResetPwdCard(props) {
 
 	const regNewpwd = register("newpassword",{
 		validate: {
-			shortInput: value => (getValues().newpassword.length >= 4) || t("ResetPwdScreen|Short password"),
+			passwordCriteria: (value) => (Object.values(validateNewPassword(value)).every(Boolean)
+			|| t('ChangePwdScreen|Password does not meet security requirements')),
 		}
 	});
 	const regNewpwd2 = register("newpassword2",{
@@ -56,7 +103,6 @@ function ResetPwdCard(props) {
 
 
 	const onSubmit = async (values) => {
-		let SeaCatAuthAPI = props.app.axiosCreate('seacat-auth');
 		let response;
 		values.pwd_token = resetPasswordCode;
 
@@ -160,50 +206,51 @@ function ResetPwdCard(props) {
 					</div>
 				</CardHeader>
 				<CardBody className="pb-1">
-					<FormGroup tag="fieldset" disabled={isSubmitting} style={{textAlign: "center"}}>
-						<h5>
-							<Label for="newpassword" style={{display: "block"}}>
-								{t('ResetPwdScreen|New Password')}
-							</Label>
-						</h5>
+					<FormGroup tag='fieldset' disabled={isSubmitting}>
+						<Label for='newpassword'>
+							{t('ChangePwdScreen|New password')}
+						</Label>
 						<Input
-							autoFocus
-							id="newpassword"
-							name="newpassword"
-							type="password"
-							invalid={errors.newpassword}
-							autoComplete="new-password"
-							required="required"
+							id='newpassword'
+							name='newpassword'
+							type='password'
+							autoComplete='new-password'
+							required='required'
+							invalid={Boolean(errors?.newpassword)}
 							onBlur={regNewpwd.onBlur}
 							innerRef={regNewpwd.ref}
 							onChange={regNewpwd.onChange}
 						/>
-						{errors.newpassword && <FormFeedback>{errors.newpassword.message}</FormFeedback>}
+						{errors?.newpassword?.type !== 'passwordCriteria'
+							&& <FormFeedback>{errors?.newpassword?.message}</FormFeedback>
+						}
+						<PasswordCriteriaFeedback
+							passwordCriteria={passwordCriteria}
+							validatePassword={validateNewPassword}
+							watchedPassword={watchedNewPassword}
+							passwordErrors={errors?.newpassword}
+						/>
 					</FormGroup>
 
-					<FormGroup tag="fieldset" disabled={isSubmitting} style={{textAlign: "center"}}>
-						<h5>
-							<Label for="newpassword2" style={{display: "block"}}>
-								{t('ResetPwdScreen|Re-enter Password')}
-							</Label>
-						</h5>
+					<FormGroup tag='fieldset' disabled={isSubmitting}>
+						<Label for='newpassword2'>
+							{t('ChangePwdScreen|Re-enter password')}
+						</Label>
 						<Input
-							id="newpassword2"
-							name="newpassword2"
-							type="password"
-							autoComplete="new-password"
-							required="required"
-							invalid={errors.newpassword2}
+							id='newpassword2'
+							name='newpassword2'
+							type='password'
+							autoComplete='new-password'
+							required='required'
+							invalid={Boolean(errors?.newpassword2)}
 							onChange={regNewpwd2.onChange}
 							onBlur={regNewpwd2.onBlur}
 							innerRef={regNewpwd2.ref}
 						/>
-
-						{errors.newpassword2 ?
-							<FormFeedback>{errors.newpassword2.message}</FormFeedback>
-							:
-							<FormText>
-								{t('ResetPwdScreen|Enter new password a second time to verify it')}
+						{errors?.newpassword2
+							? <FormFeedback>{errors?.newpassword2.message}</FormFeedback>
+							: <FormText>
+								{t('ChangePwdScreen|Enter your new password again for verification')}
 							</FormText>
 						}
 					</FormGroup>
